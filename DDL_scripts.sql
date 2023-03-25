94b38f9cd404e9fc0dd65852158e7b3a5d4af29e
huchunh <128551169+huchunh@users.noreply.github.com>
2023-03-25 16:15:17 -0400

    Update DDL_scripts.sql

--- DDL_scripts.sql
+++ DDL_scripts.sql
@@ -166,3 +166,15 @@ join transaction_device on transaction.transaction_device_id = transaction_devic
 join line on line.line_id = transaction_device.line_id
 join transit on line.transit_id = transit.transit_id) A
 group by A.YR,A.name);
+
+create or replace view passtype_year as (
+select Pass_Type_id,count(*) AS CNT, buy_year from (
+select pass_id,Pass_Type_id, EXTRACT(YEAR from transaction_time ) as buy_year from PASS 
+left join Recharge
+on pass.recharge_id=Recharge.recharge_id
+)
+group by Pass_Type_id, buy_year
+);
+
+create view  total_downtime as (
+select sum(cast (end_time as date))-sum(cast(start_time as date)) from operations where end_time is not null);
