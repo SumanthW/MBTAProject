fea919d54abc820f882a7a0cd2f20156c491f61c
Gayatri Rajendra Dalvi <dalvi.ga@northeastern.edu>
2023-03-25 01:02:13 -0400

    Added views

--- DDL_scripts.sql
+++ DDL_scripts.sql
@@ -166,3 +166,51 @@ join transaction_device on transaction.transaction_device_id = transaction_devic
 join line on line.line_id = transaction_device.line_id
 join transit on line.transit_id = transit.transit_id) A
 group by A.YR,A.name);
+
+CREATE VIEW REVENUE_YEAR_LINE AS
+select temp.line, l.name, temp.total_revenue, temp.swipe_year from(
+  SELECT td.line_id as line, SUM(t.value) as total_revenue, EXTRACT(YEAR FROM t.swipe_time) as swipe_year
+  FROM transaction t 
+  JOIN transaction_device td ON td.transaction_device_id = t.transaction_device_id
+  GROUP BY td.line_id, EXTRACT(YEAR FROM t.swipe_time)
+) temp join line l on temp.line=l.line_id;
+
+
+CREATE VIEW REVENUE_YEAR_TRANSIT AS
+select tr.name, tr.transit_id, temp2.total_revenue, temp2.swipe_year from(
+(select sum(temp.total_revenue) as total_revenue, temp.swipe_year, l.transit_id  from(
+  SELECT td.line_id as line, SUM(t.value) as total_revenue, EXTRACT(YEAR FROM t.swipe_time) as swipe_year
+  FROM transaction t 
+  JOIN transaction_device td ON td.transaction_device_id = t.transaction_device_id
+  GROUP BY td.line_id, EXTRACT(YEAR FROM t.swipe_time)
+) temp join line l on temp.line=l.line_id group by l.transit_id, temp.swipe_year))temp2 join transit tr on temp2.transit_id=tr.transit_id;
+
+
+CREATE VIEW TOP_FIVE_BUSY_STATIONS AS
+SELECT swipe_year AS YEAR,LISTAGG(name, ',') WITHIN GROUP (ORDER BY name) as CITY_NAMES
+FROM (
+  SELECT s.name, td.station_id, EXTRACT(YEAR FROM t.swipe_time) AS swipe_year, SUM(t.value) AS total_revenue,
+         ROW_NUMBER() OVER (PARTITION BY EXTRACT(YEAR FROM t.swipe_time) ORDER BY SUM(t.value) DESC) AS revenue_rank
+  FROM transaction t
+  JOIN transaction_device td ON td.transaction_device_id = t.transaction_device_id
+  JOIN station s ON s.station_id = td.station_id
+  GROUP BY s.name, td.station_id, EXTRACT(YEAR FROM t.swipe_time)
+) ranked
+WHERE revenue_rank <= 5
+group by swipe_year;
+select * from wallet_details;
+
+CREATE VIEW WALLET_DETAILS AS
+select wallet_id, wallet_type,start_date,status,pass_expiry,name as PASS_NAME, BALANCE_OR_RIDES from(
+select * from(
+(select w.wallet_id,w.wallet_type,w.wallet_expiry,w.start_date,w.status,c.card_id as ID,c.balance as BALANCE_OR_RIDES from wallet w
+join card c on  c.wallet_id = w.wallet_id union all
+select w.wallet_id,w.wallet_type, w.wallet_expiry,w.start_date,w.status, t.ticket_id as ID,t.rides as BALANCE_OR_RIDES from wallet w
+join ticket t on t.wallet_id = w.wallet_id))temp left outer join pass p on p.card_id=temp.ID and wallet_type='Card')temp2 left outer join pass_type pt on temp2.pass_type_id=pt.pass_type_id ORDER BY wallet_id;
+
+
+CREATE VIEW FRAUDULENT_RECHARGE AS 
+select r.WALLET_ID, r.RECHARGE_DEVICE_ID, r.TRANSACTION_TIME, r.RECHARGE_TYPE, rd.STATION_ID from recharge r
+join recharge_device rd on rd.recharge_device_id = r.recharge_device_id where r.recharge_device_id = NULL;
+
+/
